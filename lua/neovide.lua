13242fe205abf884e6300e2f00ebfa1e262a890f
iGxnon <igxnon@gmail.com>
2022-10-19 00:31:37 +0800

    update linux profile

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,6 +1,6 @@
 vim.cmd([[ if exists("g:neovide")
-  let g:neovide_transparency = 0.0
-  let g:transparency = 0.8
+  let g:neovide_transparency = 0.77
+  let g:transparency = 0.5
   let g:neovide_background_color = '#0f1117'.printf('%x', float2nr(255 * g:transparency))
   let g:neovide_input_macos_alt_is_meta = v:true
   let g:neovide_cursor_vfx_mode = "railgun"
